31fdd28cfc4ed4e621e6c585682fa0cb6ac2300b
lufemas <maschietto.jr@gmail.com>
2022-08-28 19:50:15 -0400

    fuly functiontal

---
+++ js/under-construction.js
@@ -0,0 +1,31 @@
+
+const $ = (selector)=>document.querySelector(selector);
+
+window.onload = ()=>{
+  
+  const params = new URLSearchParams(window.location.search)
+  
+  for (const [key, value] of params) {
+    if( key == 'logo') {
+      if(!!value) $('#logo').setAttribute('src', value);
+      continue
+    };
+    if(!!value){
+      $('#'+key).innerText = value;
+      $('#'+key).style.display = 'block';
+    }
+  }
+
+  const tel = $('#tel').innerText;
+  if(!!tel) {
+    $('#tel').setAttribute('href', 'tel:'+tel)
+    $('#tel').style.display = 'block';
+  }
+
+  const email = $('#email').innerText;
+  if(!!email) {
+    $('#email').setAttribute('href', 'mailto:'+email)
+    $('#email').style.display = 'block';
+  };
+
+}
